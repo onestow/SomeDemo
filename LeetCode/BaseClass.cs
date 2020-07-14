55c9f5e79fad51558563a626c2d21fa816567ad2
Tom <811515556@qq.com>
2020-07-14 23:15:28 +0800

    leetcode

---
+++ LeetCode/BaseClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public abstract class BaseClass
+    {
+        protected virtual void Assert<T>(T o1, T o2)
+        {
+            if (o1 is IList list1)
+            {
+                var list2 = o2 as IList;
+                if (list1.Count == list2.Count)
+                {
+                    for (int i = 0; i < list1.Count; i++)
+                        if (!list1[i].Equals(list2[i]))
+                            throw new Exception(i + ": " + list1[i] + " " + list2[i]);
+                }
+            }
+            else
+            {
+                if (o1.Equals(o2))
+                    return;
+                throw new Exception(o1 + " " + o2);
+            }
+
+        }
+
+        public abstract void Run();
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new _120().Run();
+        }
+    }
+}
